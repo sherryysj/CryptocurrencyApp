5a36e950601e7e1efae0852a9073f7416ec07006
sherryysj <45263452+sherryysj@users.noreply.github.com>
2022-01-15 21:15:23 +1100

    move Header component to Headers folder to keep files organised

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/Header';
+import Header from './src/Headers/Header';
 
 export default function App() {
   return (
