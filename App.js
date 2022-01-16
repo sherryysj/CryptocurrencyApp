548693da92890caa48e5d67fddfc82aaf764859d
sherryysj <45263452+sherryysj@users.noreply.github.com>
2022-01-16 15:17:55 +1100

    generate content component and move API request in it, update API links

--- App.js
+++ App.js
@@ -1,40 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/Headers/Header';
-import { useState, useEffect} from 'react';
-import API_Links from './util/CoinGeckoLinks.json';
+import Content from './src/Contents/Content';
 
 export default function App() {
-
-  const [coins, setCoins] = useState([]);
-  const [fetchError, setFetchError] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const fetchCoins = async () => {
-    try {
-      const response = await fetch(API_Links.CoingeckoPing);
-      if (!response.ok) throw Error('Did not receive expected data');
-      const coinList = await response.json();
-      setCoins(coinList);
-      setFetchError(null);
-    } catch (err){
-      setFetchError(err.message);
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchCoins();
-  },[])
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Header />
-      {isLoading && <Text>Loading coins...</Text>}
-      {fetchError && <Text>{fetchError}</Text>}
-      <Text>{coins.gecko_says}</Text>
+      <Content />
     </View>
   );
 }
---
+++ src/Contents/Content.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { useState, useEffect} from 'react';
+import API_Links from '../../util/CoinGeckoLinks.json';
+
+const Content = () => {
+
+    const [coins, setCoins] = useState([]);
+    const [fetchError, setFetchError] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+  
+    const fetchCoins = async () => {
+      try {
+        const response = await fetch(API_Links.CoingeckoPing);
+        if (!response.ok) throw Error('Did not receive expected data');
+        const coinList = await response.json();
+        setCoins(coinList);
+        setFetchError(null);
+      } catch (err){
+        setFetchError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+  
+    useEffect(() => {
+      fetchCoins();
+    },[])
+
+    return (
+        <View>
+            {isLoading && <Text>Loading coins...</Text>}
+            {fetchError && <Text>{fetchError}</Text>}
+            {!fetchError && !isLoading && <Text>{coins.gecko_says}</Text>}
+        </View>
+    )
+}
+
+export default Content
+
+const styles = StyleSheet.create({})
